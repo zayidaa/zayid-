3299d873b0c3b00042f9d9f012b389241fb4aa62
zayidaa <115679616+zayidaa@users.noreply.github.com>
2022-10-13 23:48:20 +0300

    add
    addcc

---
+++ index.php
@@ -0,0 +1,52 @@
+
+<html>
+<head>
+<title>TechCampus PIOW</title>
+</head>
+<body style="direction:rtl; text-align:right; background-color:#faed26;font-weight:bold;">
+
+
+<form action="print.php" method="post">
+<form action="print.php" method="post">
+  😉 الإسم <br>
+  <input type="text" name="name"><br>
+   🔒 الرقم السري <br>
+  <input type="password" name="password"><br>
+   📧 البريد <br>
+  <input type="email" name="email"><br>
+   📱 رقم الجوال <br>
+  <input type="text" name="mobile" maxlength="12"><br>
+   ❤️ افضل دورة تحبها؟ <br>
+  <input type="text" name="PIOW" value="TechCmapus PIOW" readonly>
+
+<br><br>
+
+🚻 الجنس
+<br>
+  <input type="radio" name="gender" value="male" checked> ذكر<br>
+  <input type="radio" name="gender" value="female"> أنثى<br>
+
+<br><br>
+
+<textarea name="message" rows="10" cols="30">
+TechCampus Message
+</textarea>
+
+<br><br>
+💻 أي لغات تفضل
+<br>
+  <input type="checkbox" name="lang1" value="python">Python<br>
+  <input type="checkbox" name="lang2" value="php">PHP
+
+<br><br>
+<button action="submit">التسجيل✍️</button>
+</form>
+
+
+<!-- TechCampus copyright - Start -->
+<center><a style="text-decoration: none;color:black;font-size:14px;" href="https://techcampus.com/bootcamp?piow" target="_blank"><img src="https://techcampus.sfo2.digitaloceanspaces.com/4f79892d3832f12c681344b9efdfcbfb.png" width="50px"><br>
+TechCampus - Programming in one week 🤓</a></center>
+<!-- TechCampus copyright - End -->
+
+</body>
+</html>
